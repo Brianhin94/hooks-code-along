518b4e20ca9bd905c64e84fda6d737b5811824f1
Anna Zocher <anna.zocher@gmail.com>
2021-02-08 15:34:31 -0800

    az reset to starting point for codealong so students can fork and clone

--- src/IntervalTicker.js
+++ src/IntervalTicker.js
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from 'react'
 
-function IntervalTicker(props) {
 
-    const tick = () => {console.log(props.message)}
+function IntervalTicker() {
 
-    useEffect(()=>{
-        let messageInterval = setInterval(tick, 1000)
-        return () => {
-            console.log('unmounting')
-            clearInterval(messageInterval)
-        }
-    })
 
     return (
-        <div>
-            <em>pssstt.... check the console for some interval action</em>
-        </div>
+      
     )
 }
 
