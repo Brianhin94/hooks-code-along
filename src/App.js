ecf1f8d5413669f938f163f40bf25fadcb0bf1ef
Anna Zocher <anna.zocher@gmail.com>
2021-02-08 15:58:05 -0800

    added in initial useEffect use case

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  
+  const [count, setCount] = useState(1)
+  const [user, setUser] = useState({name: "Bear"})
+
+  const increaseCount = () => {
+    setCount(count+1)
+  }
+
+  useEffect(() => {
+    // update "logged in" user
+    setUser({name: "Anna"})
+
+    // add title to my page
+    document.title = `Hello, ${user.name}`
+  }, [])
 
   return(
     <>
+      <h1>The count is: {count}</h1>
+      <button onClick={increaseCount}>Click Me!</button>
+      <h2>The user is: {user.name}</h2>
     </>
   )
 }
