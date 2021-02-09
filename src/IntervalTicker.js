c664f3eb083a1ecb7f91f4ffe6e21d4847e33583
Anna Zocher <anna.zocher@gmail.com>
2021-02-08 16:47:27 -0800

    final lab code

--- src/IntervalTicker.js
+++ src/IntervalTicker.js
@@ -1,10 +1,19 @@
+import React, { useState, useEffect } from 'react'
 
+function IntervalTicker(props) {
+    const tick = () => {console.log(`Message is: ${props.message}`)}
 
-function IntervalTicker() {
+    useEffect(() => {
+        let messageInterval = setInterval(tick, 1000)
 
+        return () =>  {
+            console.log("phew this unmounted")
+            clearInterval(messageInterval)
+        }
+    })
 
     return (
-      
+      <h1>{props.message}</h1>
     )
 }
 
