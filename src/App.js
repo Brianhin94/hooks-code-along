b1aa98b715800955e06cf8fd5a4aadfb10625131
Anna Zocher <anna.zocher@gmail.com>
2021-02-08 16:06:38 -0800

    added initial poke api call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(1)
   const [user, setUser] = useState({name: "Bear"})
+  const [pokeImg, setPokeImg] = useState("")
 
   const increaseCount = () => {
     setCount(count+1)
@@ -14,6 +16,15 @@ function App() {
 
     // add title to my page
     document.title = `Hello, ${user.name}`
+
+    // call poke api and return poke img
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${count}`)
+    .then(response => {
+      setPokeImg(response.data.sprites.front_default)
+    })
+    .catch(err => {
+      console.log(`The error is: ${err}`)
+    })
   }, [])
 
   return(
@@ -21,6 +32,7 @@ function App() {
       <h1>The count is: {count}</h1>
       <button onClick={increaseCount}>Click Me!</button>
       <h2>The user is: {user.name}</h2>
+      <img src={pokeImg} alt="pokemon" />
     </>
   )
 }
